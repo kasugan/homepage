95a97202b038f02b8edd3e50c1c10d57700406f4
sora <575860117@qq.com>
2021-07-12 18:15:55 +0800

    实现鼠标悬停时的遮罩文字浮现

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// 定义列表页
+var web_list = [
+  ["https://www.zhihu.com/", "知乎"],
+  ["http://www.bilibili.com/", "bili"],
+  ["https://cubox.pro/web/inbox", "cubox"],
+  ["https://www.wolai.com/", "我来"],
+  ["https://www.imooc.com/u/index/allcourses", "慕课"],
+  ["https://leetcode-cn.com", "力扣"],
+];
+
+let p = document.getElementsByClassName("minibox");
+let i = 0;
+for (const webKey of p) {
+  const n = `${web_list[i][0]}`;
+  webKey.onclick = () => {
+    location.href = n;
+  };
+  i++;
+}
+i = 0;
+for (const webName of p) {
+  let pName = document.createElement("p");
+  const n = `${web_list[i][1]}`;
+  pName.innerHTML = n;
+  pName.className = "radial";
+  webName.append(pName);
+  i++;
+}
